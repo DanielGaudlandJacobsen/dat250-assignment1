981fb128859c6e10cf1d1d436a8ccc03b83ee9f0
DanielGaudlandJacobsen <djacobsen5@gmail.com>
2024-11-05 03:30:04 +0100

    added input sanitation

--- social_insecurity/forms.py
+++ social_insecurity/forms.py
@@ -35,23 +35,33 @@ from wtforms.validators import (
     Length,
     ValidationError,
 )
+import bleach
 
 # Defines all forms in the application, these will be instantiated by the template,
 # and the routes.py will read the values of the fields
 
-# TODO: Add validation, maybe use wtforms.validators??
-
-# TODO: There was some important security feature that wtforms provides, but I don't remember what; implement it
-
+# Custom validator to sanitize input
+def sanitize_input(form, field):
+    # Clean the input data by stripping disallowed tags and attributes
+    field.data = bleach.clean(
+        field.data,
+        tags=[],  # Disallow all tags
+        attributes={},  # Disallow all attributes
+        strip=True  # Remove disallowed tags completely
+    )
 
 class LoginForm(FlaskForm):
     """Provides the login form for the application."""
 
     username = StringField(
-        label="Username", render_kw={"placeholder": "Username"}, validators=[DataRequired(), Length(min=3, max=25)]
+        label="Username",
+        render_kw={"placeholder": "Username"},
+        validators=[DataRequired(), Length(min=3, max=25), sanitize_input]
     )
     password = PasswordField(
-        label="Password", render_kw={"placeholder": "Password"}, validators=[DataRequired(), Length(min=6, max=100)]
+        label="Password",
+        render_kw={"placeholder": "Password"},
+        validators=[DataRequired(), Length(min=6, max=100), sanitize_input]
     )
     remember_me = BooleanField(
         label="Remember me"
@@ -62,18 +72,30 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     """Provides the registration form for the application."""
 
-    first_name = StringField(label="First Name", render_kw={"placeholder": "First Name"})
-    last_name = StringField(label="Last Name", render_kw={"placeholder": "Last Name"})
+    first_name = StringField(
+        label="First Name",
+        render_kw={"placeholder": "First Name"},
+        validators=[sanitize_input]
+    )
+    last_name = StringField(
+        label="Last Name",
+        render_kw={"placeholder": "Last Name"},
+        validators=[sanitize_input]
+    )
     username = StringField(
-        label="Username", render_kw={"placeholder": "Username"}, validators=[DataRequired(), Length(min=3, max=25)]
+        label="Username",
+        render_kw={"placeholder": "Username"},
+        validators=[DataRequired(), Length(min=3, max=25), sanitize_input]
     )
     password = PasswordField(
-        label="Password", render_kw={"placeholder": "Password"}, validators=[DataRequired(), Length(min=6, max=100)]
+        label="Password",
+        render_kw={"placeholder": "Password"},
+        validators=[DataRequired(), Length(min=6, max=100), sanitize_input]
     )
     confirm_password = PasswordField(
         label="Confirm Password",
         render_kw={"placeholder": "Confirm Password"},
-        validators=[DataRequired(), EqualTo("password", message="Passwords must match")],
+        validators=[DataRequired(), EqualTo("password", message="Passwords must match"), sanitize_input],
     )
     submit = SubmitField(label="Sign Up")
 
@@ -91,7 +113,7 @@ class PostForm(FlaskForm):
     content = TextAreaField(
         label="New Post",
         render_kw={"placeholder": "What are you thinking about?"},
-        validators=[DataRequired(), Length(max=500)],
+        validators=[DataRequired(), Length(max=500), sanitize_input],
     )
     image = FileField(label="Image")
     submit = SubmitField(label="Post")
@@ -103,7 +125,7 @@ class CommentsForm(FlaskForm):
     comment = TextAreaField(
         label="New Comment",
         render_kw={"placeholder": "What do you have to say?"},
-        validators=[DataRequired(), Length(max=300)],
+        validators=[DataRequired(), Length(max=300), sanitize_input],
     )
     submit = SubmitField(label="Comment")
 
@@ -111,18 +133,37 @@ class CommentsForm(FlaskForm):
 class FriendsForm(FlaskForm):
     """Provides the friend form for the application."""
 
-    username = StringField("Username", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired(), sanitize_input])
     submit = SubmitField("Add Friend")
 
 
 class ProfileForm(FlaskForm):
     """Provides the profile form for the application."""
 
-    education = StringField(label="Education", render_kw={"placeholder": "Highest education"})
-    employment = StringField(label="Employment", render_kw={"placeholder": "Current employment"})
-    music = StringField(label="Favorite song", render_kw={"placeholder": "Favorite song"})
-    movie = StringField(label="Favorite movie", render_kw={"placeholder": "Favorite movie"})
-    nationality = StringField(label="Nationality", render_kw={"placeholder": "Your nationality"})
+    education = StringField(
+        label="Education",
+        render_kw={"placeholder": "Highest education"},
+        validators=[sanitize_input]
+    )
+    employment = StringField(
+        label="Employment",
+        render_kw={"placeholder": "Current employment"},
+        validators=[sanitize_input]
+    )
+    music = StringField(
+        label="Favorite song",
+        render_kw={"placeholder": "Favorite song"},
+        validators=[sanitize_input]
+    )
+    movie = StringField(
+        label="Favorite movie",
+        render_kw={"placeholder": "Favorite movie"},
+        validators=[sanitize_input]
+    )
+    nationality = StringField(
+        label="Nationality",
+        render_kw={"placeholder": "Your nationality"},
+        validators=[sanitize_input]
+    )
     birthday = DateField(label="Birthday", default=datetime.now())
     submit = SubmitField(label="Update Profile")
-
